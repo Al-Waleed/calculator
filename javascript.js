ad524e25af33b38b51ce1488792b56a64ee207e2
waleed <waleedtariq47@yahoo.com>
2023-01-19 12:20:08 +0300

    Add operators and equal functionality

--- javascript.js
+++ javascript.js
@@ -20,14 +20,17 @@ function operate(a, b, c){
         return add(a, b);
     }else if (c === "-"){
         return subtract(a, b);
-    }else if (c === "*"){
+    }else if (c === "x"){
         return multiply(a, b);
     }else if (c === "/"){
         return divide(a, b);
     }
 }
 
-let displayValue = 0;
+let displayValue;
+let chosenOperator;
+let firstValue;
+let secondValue;
 const screen = document.querySelector(".screen")
 
 const numbers = document.querySelectorAll(".numbers");
@@ -49,6 +52,10 @@ const clear = document.querySelector(".clear")
 clear.addEventListener("click", () => {
     screen.textContent = 0;
     displayValue = screen.textContent;
+    displayValue = undefined;
+    chosenOperator = undefined;
+    firstValue = undefined;
+    secondValue = undefined;
     })
 const negative = document.querySelector(".negative")
 negative.addEventListener("click", () => {
@@ -72,3 +79,22 @@ decimalPoint.addEventListener("click", () => {
         displayValue = screen.textContent;
     }
 });
+
+const operators = document.querySelectorAll(".operators");
+Array.from(operators).forEach(operation =>{
+    operation.addEventListener("click", ()=> {
+        if(firstValue === undefined){
+            firstValue = displayValue;
+            chosenOperator = operation.textContent;
+            screen.textContent = 0;
+            displayValue = "";
+        }
+
+    })
+});
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    secondValue = displayValue;
+    screen.textContent = operate(firstValue,secondValue,chosenOperator);
+})
